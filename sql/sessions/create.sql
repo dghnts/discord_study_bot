94535627721a8c5747de26971b89fd7cf15f9a02
KUNIMITSU ASANO <mittsulann73@gmail.com>
2025-05-04 19:42:48 +0900

    sqlの修正

--- sql/sessions/create.sql
+++ sql/sessions/create.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS sessions(
-    id INTEGER PRIMARY KEY AUTOINCREMENT.
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     start_time TEXT,
     end_time TEXT,
     user_id TEXT,
