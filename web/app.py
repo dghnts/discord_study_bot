77f9a4eaac4ee8754a14b737d2e1b1047870a387
KUNIMITSU ASANO <mittsulann73@gmail.com>
2025-05-04 20:52:54 +0900

    エラーの修正

--- web/app.py
+++ web/app.py
@@ -1,15 +1,7 @@
 from flask import Flask, jsonify
+from web.routes import register_routes
 
-from bot import timer_service
-from service.timer_service import TimerService
-
-def create_app()
+def create_app():
     app = Flask(__name__)
-    timer_service = TimerService()
-
-    @app.route("users/<user_id>/total_minutes")
-    def total_minutes(user_id):
-        total = timer_service.get_total_time_by_user(user_id)
-        return jsonify({"user_id": user_id, "total_minutes": total or 0})
-
+    register_routes(app)
     return app
